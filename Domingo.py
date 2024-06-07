d78ac5e195605a58fe7d6d3998152c3a0cbab84a
Jansey Scofield da Silva Filho <janscofield10@gmail.com.>
2024-06-06 21:56:02 -0300

    Criando classe "Domingo"

---
+++ Domingo.py
@@ -0,0 +1,8 @@
+class Domingo:
+    def __init__(self, salmo : str, voz_1 : str, voz_2 : str, voz_3 : str, violao : str, teclado : str, bat_cajon : str):
+        self.salmo = salmo
+        self.voz_1 = voz_1
+        self.voz_2 = voz_2
+        self.voz_3 = voz_3
+        self.violao = violao
+        self.bat_cajon = bat_cajon
